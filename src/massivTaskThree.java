c8ff0904786837465754bdcebc362ed37ec5b0e4
Orda Yerkebulan <ordaerkebulan@gmail.com>
2023-02-05 16:19:29 +0600

    massiv-taskThree

---
+++ src/massivTaskThree.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class massivTaskThree {
+  public static void main(String[] args) {
+    int[] nums = {7, -3, 9, -11, 18, 99, 2, 11};
+    System.out.println("Исходный массив: " + Arrays.toString(nums));
+    System.out.print("Вторая половина элементов:");
+    for (int i = nums.length / 2; i < nums.length; i++) {
+      System.out.print(" " + nums[i]);
+    }
+  }
+}
